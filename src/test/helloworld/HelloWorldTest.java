de58fd0e1baecce03566db81f1f0ba18fb558e8b
Christophe Thibaut <cthibaut@octo.com>
2020-11-26 10:18:45 +0100

    initial commit

---
+++ src/test/helloworld/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldTest {
+    @Test
+    void testArithmeticBasics() {
+        assertEquals(4, 2+2);
+    }
+}
